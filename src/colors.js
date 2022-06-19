a321ea2c8cfc2536dca4c5b55ed3b9f981b98ae7
Maxime Moreillon <m.moreillon@gmail.com>
2022-06-19 06:28:47 +0000

    colors

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,7 @@
 export default {
-    protein: '#f29191',
-    fat: '#ede577',
-    carbohydrates: '#90de97',
+    protein: '#ed7b7b',
+    fat: '#f1e05a',
+    carbohydrates: '#41b882',
     calories: '#dfecf7',
     calorie_excess: '#ed8a8a'
 }
