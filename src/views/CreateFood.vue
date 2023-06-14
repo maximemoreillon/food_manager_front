17471d874265c39be2dd543ff8c748ee7f1e7d06
Maxime Moreillon <m.moreillon@gmail.com>
2023-06-15 07:49:05 +0900

    food creation loader

--- src/views/CreateFood.vue
+++ src/views/CreateFood.vue
@@ -1,33 +1,27 @@
 <template>
-  <v-card
-    max-width="500px"
-    class="mx-auto">
+  <v-card max-width="500px" class="mx-auto">
     <v-toolbar flat>
-      <v-btn
-        icon
-        exact
-        :to="{name: 'foods'}">
+      <v-btn icon exact :to="{ name: 'foods' }">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
       <v-toolbar-title>Create food</v-toolbar-title>
-      <v-spacer/>
+      <v-spacer />
     </v-toolbar>
-    <v-divider/>
+    <v-divider />
 
     <v-card-text>
       <v-form @submit.prevent="create_food()">
         <v-row align="baseline">
           <v-col>
-            <v-text-field
-              autofocus
-              label="name"
-              v-model="new_food.name" />
+            <v-text-field autofocus label="name" v-model="new_food.name" />
           </v-col>
           <v-col cols="auto">
-            <v-btn type="submit">Submit</v-btn>
+            <v-btn type="submit" color="primary" :loading="loading">
+              <v-icon>mdi-plus</v-icon>
+              <span>Create</span>
+            </v-btn>
           </v-col>
         </v-row>
-
       </v-form>
     </v-card-text>
   </v-card>
@@ -35,25 +29,29 @@
 
 <script>
 export default {
-  name: 'CreateFood',
+  name: "CreateFood",
 
   data: () => ({
+    loading: false,
     new_food: {
-      name: '',
-    }
-
+      name: "",
+    },
   }),
   methods: {
-    create_food(){
-      const route = `/foods`
-      this.axios.post(route,this.new_food)
-      .then(({data}) => {
-        this.$router.push({name: 'food', params: {food_id: data._id}})
-      })
-      .catch(error => {
-        console.error(error)
-      })
-    }
-  }
+    create_food() {
+      this.loading = true
+      this.axios
+        .post(`/foods`, this.new_food)
+        .then(({ data }) => {
+          this.$router.push({ name: "food", params: { food_id: data._id } })
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+  },
 }
 </script>
